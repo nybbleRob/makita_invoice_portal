bf318981b6cbe67804b0bbf6fd7a7727f25477d0
Rob <rob@nybble.co.uk>
2026-05-22 21:34:24 +0100

    Sidebar: switch Dashboard + Statements icons to Tabler filled variants
    - Dashboard: outline home (with chimney) -> IconHomeFilled
    - Statements: outline file-text -> IconFileAnalyticsFilled
    - Invoices: unchanged (already on IconFileInvoiceFilled)

--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -35,11 +35,10 @@ const Layout = () => {
       <li className={`nav-item ${location.pathname === '/' ? 'active' : ''}`}>
         <Link to="/" className="nav-link">
           <span className="nav-link-icon">
-            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-1" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+            {/* Tabler IconHomeFilled */}
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" className="icon icon-tabler icons-tabler-filled icon-tabler-home">
               <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-              <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
-              <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
-              <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
+              <path d="M12.707 2.293l9 9c.63 .63 .184 1.707 -.707 1.707h-1v6a3 3 0 0 1 -3 3h-1v-7a3 3 0 0 0 -2.824 -2.995l-.176 -.005h-2a3 3 0 0 0 -3 3v7h-1a3 3 0 0 1 -3 -3v-6h-1c-.89 0 -1.337 -1.077 -.707 -1.707l9 -9a1 1 0 0 1 1.414 0m.293 11.707a1 1 0 0 1 1 1v7h-4v-7a1 1 0 0 1 .883 -.993z" />
             </svg>
           </span>
           <span className="nav-link-title">Dashboard</span>
@@ -73,13 +72,11 @@ const Layout = () => {
         <li className={`nav-item ${location.pathname === '/statements' ? 'active' : ''}`}>
           <Link to="/statements" className="nav-link">
             <span className="nav-link-icon">
-              <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-1" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+              {/* Tabler IconFileAnalyticsFilled */}
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" className="icon icon-tabler icons-tabler-filled icon-tabler-file-analytics">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                <path d="M14 3v4a1 1 0 0 0 1 1h4" />
-                <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
-                <path d="M9 9l1 0" />
-                <path d="M9 13l6 0" />
-                <path d="M9 17l6 0" />
+                <path d="M12 2l.117 .007a1 1 0 0 1 .876 .876l.007 .117v4l.005 .15a2 2 0 0 0 1.838 1.844l.157 .006h4l.117 .007a1 1 0 0 1 .876 .876l.007 .117v9a3 3 0 0 1 -2.824 2.995l-.176 .005h-10a3 3 0 0 1 -2.995 -2.824l-.005 -.176v-14a3 3 0 0 1 2.824 -2.995l.176 -.005zm-3 10a1 1 0 0 0 -1 1v5a1 1 0 0 0 2 0v-5a1 1 0 0 0 -1 -1m3 4a1 1 0 0 0 -1 1v1a1 1 0 0 0 2 0v-1a1 1 0 0 0 -1 -1m3 -2a1 1 0 0 0 -1 1v3a1 1 0 0 0 2 0v-3a1 1 0 0 0 -1 -1" />
+                <path d="M19 7h-4l-.001 -4.001z" />
               </svg>
             </span>
             <span className="nav-link-title">Statements</span>
